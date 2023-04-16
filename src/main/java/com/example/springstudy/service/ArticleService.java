82c1cddc538fa508a86639a6627f6e489ef6934b
kimgun95 <harrison1995@naver.com>
2023-04-17 00:33:41 +0900

    사용하지 않는 import 삭제

--- src/main/java/com/example/springstudy/service/ArticleService.java
+++ src/main/java/com/example/springstudy/service/ArticleService.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityNotFoundException;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
