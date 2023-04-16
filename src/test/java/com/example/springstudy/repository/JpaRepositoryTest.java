a9761bfda0935bb9a5e5c611ae5b8bb51301b3a8
kimgun95 <harrison1995@naver.com>
2023-04-17 00:06:04 +0900

    테스트 케이스 작성 JPA 연결 테스트, 비즈니스 로직 테스트 : select, insert에 대한 테스트

---
+++ src/test/java/com/example/springstudy/repository/JpaRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.example.springstudy.repository;
+
+import com.example.springstudy.domain.Article;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Disabled
+@DisplayName("JPA 연결 테스트")
+@DataJpaTest
+public class JpaRepositoryTest {
+
+  private final ArticleRepository articleRepository;
+
+
+  public JpaRepositoryTest(@Autowired ArticleRepository articleRepository) {
+    this.articleRepository = articleRepository;
+  }
+
+
+
+  @Test
+  void JPA_게시글_SELECT() {
+    // Given
+
+    // When
+    List<Article> articles = articleRepository.findAll();
+    // Then
+    assertThat(articles)
+        .isNotNull()
+        .hasSize(30);
+  }
+
+  @Test
+  void JPA_게시글_INSERT() {
+    // Given
+    long previousCount = articleRepository.count();
+    Article article = Article.of("테스트 제목", "테스트 내용", "김건", "gunkim");
+    // When
+    Article savedArticle = articleRepository.save(article);
+    // Then
+    assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+  }
+
+
+}
---
+++ src/test/java/com/example/springstudy/service/ArticleServiceTest.java
@@ -0,0 +1,38 @@
+package com.example.springstudy.service;
+
+import com.example.springstudy.dto.ArticleDto;
+import com.example.springstudy.repository.ArticleRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.*;
+
+@DisplayName("비즈니스 로직 - Article")
+@WebMvcTest(MockitoExtension.class)
+@MockBean(JpaMetamodelMappingContext.class)
+class ArticleServiceTest {
+
+  @InjectMocks private ArticleService sut;
+  @Mock private ArticleRepository articleRepository;
+
+  @Test
+  void 게시글_리스트_조회() {
+    // Given
+    given(sut.searchArticles()).willReturn(List.of());
+    // When
+    List<ArticleDto> articles = sut.searchArticles();
+    // Then
+    assertThat(articles).isEmpty();
+  }
+
+
+}
