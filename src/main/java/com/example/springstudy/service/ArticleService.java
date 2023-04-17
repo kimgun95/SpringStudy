0dc00d63be10cf62460b89ebc066a8bc9860d9e9
kimgun95 <harrison1995@naver.com>
2023-04-17 10:48:25 +0900

    필요 없는 import 삭제

--- src/main/java/com/example/springstudy/service/ArticleService.java
+++ src/main/java/com/example/springstudy/service/ArticleService.java
@@ -6,7 +6,6 @@ import com.example.springstudy.dto.ArticleDto;
 import com.example.springstudy.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.PropertyValueException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
