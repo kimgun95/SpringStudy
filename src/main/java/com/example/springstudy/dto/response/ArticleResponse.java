a3d0e5fb9f7e7eb2c6287c7aa2171effd9ca0023
kimgun95 <harrison1995@naver.com>
2023-04-20 17:19:51 +0900

    dto와 request,response dto의 구분을 위해 ArticleResponse 생성

---
+++ src/main/java/com/example/springstudy/dto/response/ArticleResponse.java
@@ -0,0 +1,30 @@
+package com.example.springstudy.dto.response;
+
+import com.example.springstudy.domain.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ArticleResponse {
+
+  private Long id;
+  private String title;
+  private String content;
+  private String username;
+  private LocalDateTime createdAt;
+  private LocalDateTime modifiedAt;
+
+  public static ArticleResponse from (Article entity) {
+    return new ArticleResponse(
+        entity.getId(),
+        entity.getTitle(),
+        entity.getContent(),
+        entity.getUsername(),
+        entity.getCreatedAt(),
+        entity.getModifiedAt()
+    );
+  }
+}
