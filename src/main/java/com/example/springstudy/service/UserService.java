3a16b71095b189ab232068d085a2a9b464fdea2b
kimgun95 <harrison1995@naver.com>
2023-04-20 14:34:51 +0900

    UserService에 회원가입 비즈니스 로직 생성 - username, password null 체크 - 같은 이름의 username이 있는지 체크

--- src/main/java/com/example/springstudy/service/UserService.java
+++ src/main/java/com/example/springstudy/service/UserService.java
@@ -8,10 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class UserService {
 
@@ -25,14 +27,27 @@ public class UserService {
     if (username == null || password == null)
       throw new IllegalArgumentException("로그인 실패, null 값을 받고 있습니다.");
 
-    UserAccount getUser = userRepository.findByUsername(username).orElseThrow(
+    final UserAccount getUser = userRepository.findByUsername(username).orElseThrow(
         () -> new EntityNotFoundException("등록된 사용자가 없습니다.")
     );
 
     if (!getUser.getPassword().equals(password))
-      throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+      throw new SecurityException("비밀번호가 일치하지 않습니다.");
 
     response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(getUser.getUsername()));
+    return new UserAccountResponse("회원가입 성공", 200);
+  }
+
+  public UserAccountResponse signup(final UserAccount userAccount) {
+    final String username = userAccount.getUsername();
+    final String password = userAccount.getPassword();
+    if (username == null || password == null)
+      throw new IllegalArgumentException("로그인 실패, null 값을 받고 있습니다.");
+
+    if (userRepository.findByUsername(username).isPresent())
+      throw new EntityExistsException("중복된 사용자가 존재합니다.");
+
+    userRepository.save(userAccount);
     return new UserAccountResponse("회원가입 성공", 200);
   }
 }
