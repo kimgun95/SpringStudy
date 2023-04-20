554c049ec8d0ef19c5d86ba088f7d403e9498507
kimgun95 <harrison1995@naver.com>
2023-04-20 14:08:48 +0900

    UserController의 로그인 api 생성

--- src/main/java/com/example/springstudy/controller/UserController.java
+++ src/main/java/com/example/springstudy/controller/UserController.java
@@ -1,14 +1,26 @@
 package com.example.springstudy.controller;
 
+import com.example.springstudy.dto.request.UserAccountRequest;
+import com.example.springstudy.dto.response.UserAccountResponse;
 import com.example.springstudy.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @RestController
 public class UserController {
 
   private final UserService userService;
+
+  @PostMapping("/login")
+  public ResponseEntity<UserAccountResponse> login(@RequestBody UserAccountRequest userAccountRequest, HttpServletResponse response) {
+    return ResponseEntity.ok(userService.login(UserAccountRequest.toEntity(userAccountRequest), response));
+  }
 
 
 }
