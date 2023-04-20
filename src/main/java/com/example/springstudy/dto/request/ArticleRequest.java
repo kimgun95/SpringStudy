3f1c3b8941472fddebd56613fdd9c063562b5c75
kimgun95 <harrison1995@naver.com>
2023-04-21 03:13:14 +0900

    필요 없는 구현 삭제 - ArticleRequest

--- src/main/java/com/example/springstudy/dto/request/ArticleRequest.java
+++ src/main/java/com/example/springstudy/dto/request/ArticleRequest.java
@@ -9,10 +9,4 @@ public class ArticleRequest {
   private String title;
   private String content;
 
-//  public static Article toEntity(ArticleRequest articleRequest) {
-//    return Article.of(
-//        articleRequest.getTitle(),
-//        articleRequest.getContent()
-//    );
-//  }
 }
