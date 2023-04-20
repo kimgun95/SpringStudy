c4a92cd3642bef09a3e5d66f4a8a89bacd8eea21
kimgun95 <harrison1995@naver.com>
2023-04-20 14:12:57 +0900

    UserRepository에 유저이름으로 UserAccount 찾는 메서드 추가

--- src/main/java/com/example/springstudy/repository/UserRepository.java
+++ src/main/java/com/example/springstudy/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.springstudy.repository;
 import com.example.springstudy.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserAccount, Long> {
+  Optional<UserAccount> findByUsername(String username);
 }
