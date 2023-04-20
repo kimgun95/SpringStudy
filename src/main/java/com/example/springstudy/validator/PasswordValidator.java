0da6742fff0ad3954a6ae9038a209161b7126eca
kimgun95 <harrison1995@naver.com>
2023-04-20 15:37:58 +0900

    PasswordValidator 생성 - 각각 최소 1회 이상의 소문자(a-z), 대문자(A-Z), 숫자(0-9)를 사용한 password을 검증한다. - 글자수 제한은 8~15 이다.

---
+++ src/main/java/com/example/springstudy/validator/PasswordValidator.java
@@ -0,0 +1,18 @@
+package com.example.springstudy.validator;
+
+import com.example.springstudy.annotation.Password;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidator implements ConstraintValidator<Password, String>  {
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,15}$");
+    Matcher matcher = pattern.matcher(value);
+    return matcher.matches();
+  }
+}
