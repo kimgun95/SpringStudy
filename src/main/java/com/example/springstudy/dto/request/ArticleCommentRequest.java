4d156b7e59f747c4b8eee4dd523078facdfb3f51
kimgun95 <harrison1995@naver.com>
2023-04-24 12:12:37 +0900

    comment dto의 유효성 검증 어노테이션 수정 댓글의 request dto에 게시글 ID가 있는 것은 이상한 설계이다. 이유는 댓글 수정시에는 게시글 ID를 요청으로 보내고 있지 않기 때문이다. 추후 게시글의 ID를 url path를 통해 받아오는 방향으로 재설계 해야할 필요가 있다. 따라서 @Valid를 save, update 요청시 dto에 모두 적용하기 위해 articleId에는 @NotNull을 제거한다.

--- src/main/java/com/example/springstudy/dto/request/ArticleCommentRequest.java
+++ src/main/java/com/example/springstudy/dto/request/ArticleCommentRequest.java
@@ -2,14 +2,11 @@ package com.example.springstudy.dto.request;
 
 import lombok.Getter;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Getter
 public class ArticleCommentRequest {
 
-  @NotNull
-  @Min(0)
   private Long articleId;
   @NotNull
   private String content;
