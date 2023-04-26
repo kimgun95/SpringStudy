16d3b179fbb924517ca9128b73ad94db9b1ad432
kimgun95 <harrison1995@naver.com>
2023-04-26 15:55:31 +0900

    UserAccountRole에 authority 필드 추가

--- src/main/java/com/example/springstudy/domain/constant/UserAccountRole.java
+++ src/main/java/com/example/springstudy/domain/constant/UserAccountRole.java
@@ -1,6 +1,18 @@
 package com.example.springstudy.domain.constant;
 
 public enum UserAccountRole {
-  USER,
-  ADMIN
+  USER("ROLE_USER"),
+  ADMIN("ROLE_ADMIN")
+  ;
+
+  private final String authority;
+
+  UserAccountRole(String authority) {
+    this.authority = authority;
+  }
+
+  public String getAuthority() {
+    return this.authority;
+  }
+
 }
