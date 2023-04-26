5517e33e26e88818ba5249510ff528c6899090a7
kimgun95 <harrison1995@naver.com>
2023-04-26 15:56:37 +0900

    controller에 인증된 유저 정보를 받기 위해 @AuthenticationPrincipal 파라미터 추가

--- src/main/java/com/example/springstudy/controller/ArticleCommentController.java
+++ src/main/java/com/example/springstudy/controller/ArticleCommentController.java
@@ -4,12 +4,13 @@ import com.example.springstudy.dto.ArticleCommentDto;
 import com.example.springstudy.dto.request.ArticleCommentRequest;
 import com.example.springstudy.dto.response.ArticleCommentResponse;
 import com.example.springstudy.dto.response.StatusResponse;
+import com.example.springstudy.security.UserDetailsImpl;
 import com.example.springstudy.service.ArticleCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
@@ -20,29 +21,29 @@ public class ArticleCommentController {
 
   @PostMapping("/articles/{articleId}/comments")
   public ResponseEntity<ArticleCommentResponse> saveComment(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
       @PathVariable Long articleId,
-      @RequestBody @Valid ArticleCommentRequest articleCommentRequest,
-      HttpServletRequest request
+      @RequestBody @Valid ArticleCommentRequest articleCommentRequest
   ) {
-    return ResponseEntity.ok(articleCommentService.saveComment(articleId, ArticleCommentDto.from(articleCommentRequest), request));
+    return ResponseEntity.ok(articleCommentService.saveComment(userDetails.getUser(), articleId, ArticleCommentDto.from(articleCommentRequest)));
   }
 
   @PutMapping("/articles/{articleId}/comments/{commentId}")
   public ResponseEntity<ArticleCommentResponse> updateComment(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
       @PathVariable Long articleId,
       @PathVariable Long commentId,
-      @RequestBody @Valid ArticleCommentRequest articleCommentRequest,
-      HttpServletRequest request
+      @RequestBody @Valid ArticleCommentRequest articleCommentRequest
   ) {
-    return ResponseEntity.ok(articleCommentService.updateComment(articleId, commentId, ArticleCommentDto.from(articleCommentRequest), request));
+    return ResponseEntity.ok(articleCommentService.updateComment(userDetails.getUser(), articleId, commentId, ArticleCommentDto.from(articleCommentRequest)));
   }
 
   @DeleteMapping ("/articles/{articleId}/comments/{commentId}")
   public ResponseEntity<StatusResponse> deleteComment(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
       @PathVariable Long articleId,
-      @PathVariable Long commentId,
-      HttpServletRequest request
+      @PathVariable Long commentId
   ) {
-    return ResponseEntity.ok(articleCommentService.deleteComment(articleId, commentId, request));
+    return ResponseEntity.ok(articleCommentService.deleteComment(userDetails.getUser(), articleId, commentId));
   }
 }
--- src/main/java/com/example/springstudy/controller/ArticleController.java
+++ src/main/java/com/example/springstudy/controller/ArticleController.java
@@ -4,12 +4,13 @@ import com.example.springstudy.dto.ArticleDto;
 import com.example.springstudy.dto.request.ArticleRequest;
 import com.example.springstudy.dto.response.ArticleResponse;
 import com.example.springstudy.dto.response.StatusResponse;
+import com.example.springstudy.security.UserDetailsImpl;
 import com.example.springstudy.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -31,27 +32,27 @@ public class ArticleController {
 
   @PostMapping("/articles")
   public ResponseEntity<ArticleResponse> saveArticle(
-      @RequestBody @Valid ArticleRequest articleRequest,
-      HttpServletRequest request
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @RequestBody @Valid ArticleRequest articleRequest
   ) {
-    return ResponseEntity.ok(articleService.saveArticle(ArticleDto.from(articleRequest), request));
+    return ResponseEntity.ok(articleService.saveArticle(userDetails.getUser(), ArticleDto.from(articleRequest)));
   }
 
   @PutMapping("/articles/{articleId}")
   public ResponseEntity<ArticleResponse> updateArticle(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
       @PathVariable Long articleId,
-      @RequestBody @Valid ArticleRequest articleRequest,
-      HttpServletRequest request
+      @RequestBody @Valid ArticleRequest articleRequest
   ) {
-    return ResponseEntity.ok(articleService.updateArticle(articleId, ArticleDto.from(articleRequest), request));
+    return ResponseEntity.ok(articleService.updateArticle(userDetails.getUser(), articleId, ArticleDto.from(articleRequest)));
   }
 
   @DeleteMapping ("/articles/{articleId}")
   public ResponseEntity<StatusResponse> deleteArticle(
-      @PathVariable Long articleId,
-      HttpServletRequest request
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @PathVariable Long articleId
   ) {
-    return ResponseEntity.ok(articleService.deleteArticle(articleId, request));
+    return ResponseEntity.ok(articleService.deleteArticle(userDetails.getUser(), articleId));
   }
 
 }
